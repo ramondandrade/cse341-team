78cffeaa05f9792d7097290f5374adccfcfcc8e5
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-10-09 15:34:51 -0600

    update 7

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -23,17 +23,16 @@ const getItemsByCharacterId = async (req, res) => {
       return res.status(400).json({ message: "Character ID is required" });
     }
 
-    const items = await mongodb.getDatabase().db().collection("inventory").find({ characterId: id });
+    const result = await mongodb.getDatabase().db().collection("inventory").find({ characterId: id });
     result.toArray().then((items) => {
+      if (items.length === 0) {
+        return res.status(404).json({ message: "No items found for this character" });
+      }
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(items);
     });
-    
-    if (items.length === 0) {
-      return res.status(404).json({ message: "No items found for this character" });
-    }
-
-    res.status(200).json(items);
+  
+    // res.status(200).json(items);
   } catch (error) {
     console.error("Error fetching items by character ID:", error);
     res.status(500).json({
@@ -127,7 +126,7 @@ const createItem = async (req, res) => {
     };
     const response = await mongodb.getDatabase().db().collection("inventory").insertOne(item);
     if (response.acknowledged) {
-      res.status(204).send();
+      res.status(201).json({ message: 'Item created successfully', character: item });
     } else {
       res.status(500).json(response.error || "Some error occurred while updating the item.");
     }
@@ -210,7 +209,7 @@ const updateItem = async (req, res) => {
       .collection("inventory")
       .replaceOne({ _id: itemId }, item);
     if (response.modifiedCount > 0) {
-      res.status(204).send();
+      res.status(200).json({ message: 'Item updated successfully', character: item });
     } else {
       res.status(500).json(response.error || "Some error occurred while updating the item.");
     }
