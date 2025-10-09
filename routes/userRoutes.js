85c2ebd48699d8b08fbc18a5764580b06e1799f3
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-10-09 14:20:48 -0600

    ry-8

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const validation = require("../middleware/userValidate");
+const isAuthenticated = require("../middleware/authenticate");
 
+// GET /player/ - Get all users
 router.get("/", userController.getAllUsers);
+
+// GET /player/{id} - Get user by ID
 router.get("/:id", userController.getSingleUser);
-router.post("/", validation.saveUser, userController.createUser);
-router.put("/:id", validation.saveUser, userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+
+// POST /player/ - Create a new user
+router.post("/", isAuthenticated, validation.saveUser, userController.createUser);
+
+// PUT /player/{id} - Update user by ID
+router.put("/:id", isAuthenticated, validation.saveUser, userController.updateUser);
+
+// DELETE /player/{id} - Delete user by ID
+router.delete("/:id", isAuthenticated, userController.deleteUser);
 
 module.exports = router;
