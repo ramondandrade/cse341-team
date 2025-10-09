41a9aa01e038276cde7ec6cf275315afcdf9a3a5
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-10-09 14:21:41 -0600

    added inventory route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 router.use('/character', require('./characterRoutes'));
 router.use("/player", require("./userRoutes"));
 router.use("/quest", require("./questRoutes"));
+router.use("/inventory", require("./itemRoutes"));
 router.use("/", require("./swagger"));
 router.use('/auth', passport.authenticate('github')); 
 router.use('/logout', (req, res) => {
