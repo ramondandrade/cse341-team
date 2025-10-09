b1d9e9212bd3b94313e5cc17f6f72558ad6eb164
R-vanLangeveld <rvanlangeveld@byupathway.edu>
2025-10-09 14:29:04 -0600

    updats

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,23 +1,24 @@
 const router = require("express").Router();
 const itemController = require("../controllers/itemController");
 const validation = require("../middleware/itemValidate");
+const isAuthenticated = require("../middleware/authenticate");
 
 // GET /inventory/ - Get all items
-router.get("/", itemController.getAllItems);
+router.get("/", isAuthenticated, itemController.getAllItems);
 
 // GET /inventory/{id} - Get item by ID
-router.get("/:id", itemController.getSingleItem);
+router.get("/:id", isAuthenticated, itemController.getSingleItem);
 
 // GET /inventory/user/{characterId} - Get items by character ID
-router.get('/character/:characterId', itemController.getItemsByCharacterId);
+router.get('/character/:characterId', isAuthenticated, itemController.getItemsByCharacterId);
 
 // POST /inventory/ - Create a new item
-router.post("/", validation.saveItem, itemController.createItem);
+router.post("/", isAuthenticated, validation.saveItem, itemController.createItem);
 
 // PUT /inventory/{id} - Update item by ID
-router.put("/:id", validation.saveItem, itemController.updateItem);
+router.put("/:id", isAuthenticated, validation.saveItem, itemController.updateItem);
 
 // DELETE /inventory/{id} - Delete item by ID
-router.delete("/:id", itemController.deleteItem);
+router.delete("/:id", isAuthenticated, itemController.deleteItem);
 
 module.exports = router;
